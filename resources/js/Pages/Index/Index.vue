7c6615065e95026416bb47d3e6a84327ac1ea205
Monsieur Cheval <girardvprog@gmail.com>
2025-04-13 08:20:31 -0400

    init

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Heello {{ counter }}</h1>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+const counter = ref(0);
+
+ setInterval(() => counter.value++, 1000);
+
+
+</script>
